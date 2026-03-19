6c8cebab0f1ed6f616f8f2a73dc94f8ce3aa66d5
patrick red <securitysystemf@gmail.com>
2026-03-19 08:49:54 -0600

    Minimal server test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is working");
+  res.send("Server is alive");
 });
 
 const PORT = process.env.PORT || 3000;
